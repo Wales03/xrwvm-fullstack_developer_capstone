831da7345520f91605a0c0c535fdc8c2548bf3ba
Wales03 <danielwales003@gmail.com>
2024-10-31 09:20:43 -0400

    added models for dealership

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name')
+    ordering = ('car_make', 'name')
+    raw_id_fields = ('car_make',)
 
-# CarModelInline class
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'website')
+    search_fields = ('name', 'country')
+    inlines = [CarModelInline]
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
